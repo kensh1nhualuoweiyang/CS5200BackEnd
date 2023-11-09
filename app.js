d6a2081cd0dbe256902dcff72c372958ec7829b4
Chen <kensh1nhualuoweiyang@gmail.com>
2023-11-09 17:55:57 -0500

    set-up env

---
+++ app.js
@@ -0,0 +1,8 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+const app = express()
+app.use(cors)
+app.use(cookieParser)
+app.use(express.json)
+app.listen(4000)
